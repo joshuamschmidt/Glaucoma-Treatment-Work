bebf8fc3b9eeed9bb1a120295225fb572e6d6c55
joshuamschmidt <joshmschmidt1@gmail.com>
2023-06-09 21:10:54 +0930

    no closing parentheses in file import

--- treatment_initiation_master_script_2023_01_08.R
+++ treatment_initiation_master_script_2023_01_08.R
@@ -6,12 +6,12 @@ library("stringr")
 library("readr")
 library("lubridate")
 
-## load the spreadsheets : ## 
+## load the spreadsheets : ##
 dir =""
-clinical_data <- read_csv(paste0(dir,"progressa_clinical_data_2021_11_22.csv") #load clinical df
-demographic_details<- read_csv(paste0(dir,"progressa_demographic_details_2021_11_22.csv")#used for demographic data and IDs
- 
-## List generic medication names and their drug names:  ##                          
+clinical_data <- read_csv(paste0(dir,"progressa_clinical_data_2021_11_22.csv")) #load clinical df
+demographic_details<- read_csv(paste0(dir,"progressa_demographic_details_2021_11_22.csv")) #used for demographic data and IDs
+
+## List generic medication names and their drug names:  ##
 Timolol<- c( "COMBIGAN", "TIMOLOL","XALACOM", "COSOPT", "NYOGEL", "DUOTRAV", "GANFORT", "TIMOPTOL","TIMOPTIC","AZARGA", "AZAGA", "AZARGRA")
 Brimodine <-c( "SIMBRINZA", "ALPHAGAN")
 Betaxolol <-c("BETOPIC","BETOPTIC")
@@ -31,7 +31,7 @@ demographics<- demographic_details%>%
   mutate(prog_id =as.numeric(str_remove(ID, "PROG")))%>%
   select(prog_id, Gender, DOB,Ethnicity,FamilyHx)%>%
   mutate(FamilyHx=toupper(FamilyHx),Ethnicity=as.factor(Ethnicity),
-         FamilyHx = ifelse(str_detect(FamilyHx, "RECORD")==TRUE, NA, 
+         FamilyHx = ifelse(str_detect(FamilyHx, "RECORD")==TRUE, NA,
                            ifelse(FamilyHx=="NO", FALSE,  ifelse(FamilyHx=="YES", TRUE, NA))))
 
 ## Prepare relevant baseline covariates : ##
@@ -58,64 +58,64 @@ covariates_compiled<-clinical_data%>%
   left_join(x=., y= demographics, by= "prog_id")%>%
   left_join(x=., y= baseline_covariates, by= "prog_id")%>%
   arrange(prog_id)
-  
+
 
 ## Construct spreadsheet for summary of treatment data : ##
 colnames(clinical_data) = gsub("[[:space:]]", "\\_", colnames(clinical_data))
 medication_spreadsheet<-clinical_data%>%
-  
-  #Prepare spreadsheet : 
+
+  #Prepare spreadsheet :
   #topical data was inputed into either a column called 'glc medication' or into several columns called 'RE medication1'. Could not uniformly use one of another, so have to concatenate them together
   mutate(GLC_Medication= ifelse(is.na(GLC_Medication)==TRUE, paste0(`RE_Medctn_1`,", ", `RE_Medctn_2`,", ", `LE_Medctn_1`,", ", `LE_Medctn_2`), GLC_Medication),
          GLC_Medication= ifelse(str_detect(GLC_Medication, "NA")==TRUE, "NIL", GLC_Medication), #If blank, indicate no treatment
- 
+
          GLC_Medication= toupper(GLC_Medication), #convert to upper case for uniformity
          SLTOD_pd=toupper(SLTOD_pd),SLTOS_pd=toupper(SLTOS_pd),TRABOD_pd = toupper(TRABOD_pd),TRABOS_pd = toupper(TRABOS_pd), #convert SLT and Trab data to upper case
-         
+
          prog_id = as.numeric(str_extract(VisitID, "\\d{4}(?=\\.)")))%>% #convert visit ids to PROGRESSA IDs (Visit IDs are of the form: paste0("PROG ID Number",".","Visit Number") - i.e. visit 1 PROGRESSA ID 1 = 0001.01)
-  
+
   filter(str_detect(VisitID, "\\.00")==FALSE)%>% #sometimes a row is inputted at the time of ? enrolment- but no other parameters are included - remove these
- 
+
   #select relevant column names
   select(prog_id,VisitID,Visit_Date,GLC_Medication,SLTOD_pd,SLTOS_pd,TRABOD_pd,TRABOS_pd)%>% #I've never known what the '_pd' on the PROGRESSA df means
-  
-  #Extract medication data : 
-  #Use stringr to detect whether a patient was on a medication at a given time point. Medications were inputted as generic names, so I've converted them to drug names. 
+
+  #Extract medication data :
+  #Use stringr to detect whether a patient was on a medication at a given time point. Medications were inputted as generic names, so I've converted them to drug names.
   mutate(Brimodine = as.logical(str_detect(string = GLC_Medication,pattern= paste(Brimodine,collapse="|"))),
-         
+
          Timolol = as.logical(str_detect(string = GLC_Medication,pattern= paste(Timolol,collapse="|"))),
          Betaxolol  = as.logical(str_detect(string = GLC_Medication,pattern= paste(Betaxolol,collapse="|"))),
-         
+
          Acetazolamide= as.logical(str_detect(string = GLC_Medication,pattern= paste(Acetazolamide,collapse="|"))),
          Brinzolamide = as.logical(str_detect(string = GLC_Medication,pattern= paste(Brinzolamide,collapse="|"))),
          Dorzolamide = as.logical(str_detect(string = GLC_Medication,pattern= paste(Dorzolamide,collapse="|"))),
-         
+
          Brimatoprost = as.logical(str_detect(string = GLC_Medication,pattern= paste(Brimatoprost,collapse="|"))),
          Travoprost = as.logical(str_detect(string = GLC_Medication,pattern= paste(Travoprost,collapse="|"))),
          Latanoprost = as.logical(str_detect(string = GLC_Medication,pattern= paste(Latanoprost,collapse="|"))),
          Tafluprost = as.logical(str_detect(string = GLC_Medication,pattern= paste(Tafluprost,collapse="|"))),
-         
+
          #SLT data is of the form : Yes or No:
          SLT = as.logical(SLTOD_pd=="YES"|SLTOS_pd=="YES"),
          Trab = as.logical(TRABOD_pd=="YES"|TRABOS_pd=="YES"),
-         
-         #Calculate number of agents that a patient is on at a given timepoint. 
+
+         #Calculate number of agents that a patient is on at a given timepoint.
          agent_number = Brimodine+ Timolol+ Betaxolol+Acetazolamide+Brinzolamide+Dorzolamide+Brimatoprost+Travoprost+Latanoprost+Tafluprost+SLT+Trab,
-         
+
          #Determine if someone is one treatment at a time point
          treatment = as.logical(agent_number==0))%>%
-  
+
   #Calculate some of the treatment summary statistics:
   group_by(prog_id)%>%
   mutate(max_agents = max(agent_number), #maximum number of agents (including SLT/Trab) at any time point
          SLT_during_monitoring = ifelse(sum(SLT=="TRUE")>0|sum(SLTOS_pd=="YES")>0, TRUE, FALSE), #To know if patient had an SLT at all during monitoring
          TRAB_during_monitoring = ifelse(sum(TRABOD_pd=="YES")>0|sum(TRABOS_pd=="YES")>0,TRUE, #To know if patient had a Trab at all during monitoring
                                          ifelse(sum(TRABOD_pd=="YES")==0&sum(TRABOS_pd=="YES")==0&sum(TRABOD_pd=="NO")==0&sum(TRABOS_pd=="NO")==0,NA,FALSE)))%>%
-  
-  #Then assess if a patient had treatment increase during monitoring of not: 
+
+  #Then assess if a patient had treatment increase during monitoring of not:
   mutate(baseline_treated=dplyr::first(treatment),#?treated at enrolment
         increase_rx = agent_number> dplyr::first(agent_number),
-        increase_rx_during_monitoring =ifelse(sum((agent_number >  dplyr::first(agent_number))==TRUE) > 0,TRUE, FALSE))%>% #determines if at any time point, the patient had an increase in treatment. 
+        increase_rx_during_monitoring =ifelse(sum((agent_number >  dplyr::first(agent_number))==TRUE) > 0,TRUE, FALSE))%>% #determines if at any time point, the patient had an increase in treatment.
   ungroup()
 
 ## calculate date for commencement of treatment ----
@@ -134,9 +134,9 @@ treatment_increase_date<-medication_spreadsheet%>%
   mutate(increase_date = dplyr::first(Visit_Date))%>%
   select(prog_id, baseline_treated,Visit_Date,increase_date)%>%
   filter(duplicated(prog_id)==FALSE)%>%
-  select(-Visit_Date,-baseline_treated)  
+  select(-Visit_Date,-baseline_treated)
 
-## Compile the final  medication spreadsheet 
+## Compile the final  medication spreadsheet
 complete_medication_spreadsheet<-medication_spreadsheet%>%
   left_join(x=., y = treatment_start, by= "prog_id")%>%
   left_join(x=., y = treatment_increase_date, by= "prog_id")%>%
@@ -145,7 +145,7 @@ complete_medication_spreadsheet<-medication_spreadsheet%>%
   rename(baseline_date= Visit_Date,baseline_agent_number=agent_number)%>%
   mutate(time_to_increase= ifelse(increase_rx_during_monitoring ==TRUE, as.numeric(dmy(increase_date) - dmy(baseline_date))/365.25,
          as.numeric(dmy("12/11/2021") - dmy(baseline_date))/365.25))%>%
-  
+
   #Stipulate that treatment initiation must occur within 5 years of enrolment
   mutate(increase_rx_during_monitoring_censored=ifelse(time_to_increase> 5,FALSE,increase_rx_during_monitoring ),
          time_to_increase_censored=ifelse(time_to_increase>5,5,time_to_increase))%>%
